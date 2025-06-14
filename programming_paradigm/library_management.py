0ec08ebcb43ce7d20f729049753c9e53ff066fde
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-06-14 08:52:19 +0000

    Implement Basic OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,53 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+    
+    
+class Library:
+    def __init__(self):
+        self._books = []  # private list of Book objects
+
+    def add_book(self, book):
+        """Add a Book object to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                print(f"'{title}' has been checked out.")
+                return
+        print(f"Sorry, '{title}' is not available.")
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                print(f"'{title}' has been returned.")
+                return
+        print(f"'{title}' is not checked out or does not exist.")
+
+    def list_available_books(self):
+        """Print all books that are currently available."""
+        available = [f"{book.title} by {book.author}" for book in self._books if book.is_available()]
+        if available:
+            for book in available:
+                print(book)
+        else:
+            print("No books available.")
